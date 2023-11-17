67b2032d5ed5e7844e8fe67d3aea1162b976976e
pinion <shalompmc0505@naver.com>
2023-11-17 12:23:30 +0900

    refactor: Extract validation regex to object

--- src/user/validation.utils.ts
+++ src/user/validation.utils.ts
@@ -1,15 +1,22 @@
+export const reg = {
+  hasUpperCase: new RegExp(/[A-Z]/),
+  hasNumber: new RegExp(/\d/),
+  hasSpecialCharacter: new RegExp(/[!@#$%^&*(),.?":{}|<>]/),
+  emailCheck: new RegExp(/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z]{2,}$/),
+};
+
 export const validateEmail = (email: string): boolean => {
-  const emailCheckPatton = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
-  return emailCheckPatton.test(email);
+  return reg.emailCheck.test(email);
 };
 
 export const validatePassword = (password: string) => {
-  const hasUpperCase = /[A-Z]/.test(password);
-  const hasNumber = /\d/.test(password);
-  const hasSpecialCharacter = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-  return hasUpperCase && hasNumber && hasSpecialCharacter;
+  const { hasNumber, hasSpecialCharacter, hasUpperCase } = reg;
+  const regTests = [hasNumber, hasSpecialCharacter, hasUpperCase].map((reg) =>
+    reg.test(password),
+  );
+  return regTests.every((test) => test === true);
 };
 
 export const validateName = (name: string) => {
-  return name.length < 16;
+  return name.length < 16 && name.length > 0;
 };
