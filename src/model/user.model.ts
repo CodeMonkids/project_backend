6a0753f827813abd652483f5e48289f785ace2d4
pinion <shalompmc0505@naver.com>
2023-11-25 02:14:28 +0900

    reafactor: Remove unused model

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,15 +1,3 @@
-export interface Userinterface {
-  name: string;
-  email: string;
-  password: string;
-}
-export class User {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-}
-
 export interface SignupBody {
   name: string;
   email: string;
